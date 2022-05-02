9ffab4c3e319886bc6e58d9cd2686335e7aad26b
xDrag10 <matteobianchi2100@gmail.com>
2022-05-02 16:11:56 +0200

    Query n.2 Aggiunta

--- EserciziAnagrafe.sql
+++ EserciziAnagrafe.sql
@@ -1,7 +1,13 @@
-use anagrafe;
-
 /*persone maggiorenni, con nome e regione di nascita*/
 select p.Nome, c.Regione
 from persona p, città c
 where p.Età > 17 and (p.CittàNascita = c.Nome)
+
+/*coppie <genitore,figlio> nati nella stessa città*/
+
+select genia.Genitore, genia.Figlio
+from persona pf join genia on pf.Nome = genia.Figlio
+    join persona pg on pg.Nome = genia.Genitore
+where pf.CittàNascita = pg.CittàNascita
+
 
