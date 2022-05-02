1272bf8de4848d5e156af694d81f658d447ddf75
xDrag10 <matteobianchi2100@gmail.com>
2022-05-02 12:37:18 +0200

    Query Titolo romanzi autori 20esimo secolo added

--- EserciziLibreria.sql
+++ EserciziLibreria.sql
@@ -6,4 +6,12 @@ from autore a, romanzo r
 where r.Autore = a.ID and (a.Nazionalità = 'inglese')
 order by a.Cognome
 
-/**/
+/*Titolo"dei"romanzi"di"autori"nati"nel"ventesimo"secolo,"ordinati"per"cognome"e"nome"
+dell’autore"e"–"a"parità"di"autore"–"di"data"di"pubblicazione*/
+
+select r.Titolo
+from romanzo r, autore a
+where r.Autore = a.ID and (a.DataNascita > 1899 and a.DataNascita < 2000)
+order by a.Cognome, r.AnnoPubblicazione
+
+
