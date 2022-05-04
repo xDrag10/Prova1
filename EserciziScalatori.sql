db836f6d99270b1c6617bff2e4bc1fcb6360d0f4
xDrag10 <matteobianchi2100@gmail.com>
2022-05-04 11:59:23 +0200

    Query n.2 aggiunta

--- EserciziScalatori.sql
+++ EserciziScalatori.sql
@@ -5,3 +5,14 @@ effettuato una scalata.*/
 select s.CF, n.Continente
 from scalatore s join scalata t on s.CF = t.Scalatore
     join nazione n on n.Nome = t.Nazione
+
+/*Per ogni scalatore nato prima del 1980, calcolare
+tutti i continenti in cui ha effettuato una scalata,
+ordinando il risultato per codice fiscale e, a parità di
+codice fiscale, per il nome del continente.*/
+
+select n.Continente, s.CF
+from scalatore s join scalata t on s.Cf = t.Scalatore
+    join nazione n on n.Nome = t.nazione
+where s.annoNascita < 1980
+order by s.CF, n.continente
